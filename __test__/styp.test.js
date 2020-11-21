8847a56e89c4f257e6dfea95a3b9885f2299e0aa
Archan Patkar <pat@archanpatkar.com>
2020-11-21 21:02:10 +0530

    tests

--- __test__/styp.test.js
+++ __test__/styp.test.js
@@ -1,39 +1,47 @@
+const { tagged, sum } = require('../dist/styp.min')
 
-const Point3D = tagged("Point3D", ["x","y","z"]);
-console.log(Point3D.toString());
-const a = Point3D(1, 2, 3) // { x: 1, y: 2, z: 3 }
-console.log(a.x == 1 && a.y == 2 && a.z == 3) // true
-console.log(a.toString()) // 'Point3D(1, 2, 3)'
-console.log(Point3D.is(a)) // true
-Point3D.prototype.scale = function(n){
-  return Point3D(this.x * n, this.y * n, this.z * n)
-}
-const b = a.scale(2) // { x: 2, y: 4, z: 6 }
-console.log(b.toString()) // 'Point3D(2, 4, 6)'
-const c = Point3D.from({y: 2, x: 1, z: 3}) // { x: 1, y: 2, z: 3 }
-console.log(c.toString())
+// add more tests
+test('styp.tagged', () => {
+    const Point3D = tagged("Point3D", ["x","y","z"])
+    const a = Point3D(1, 2, 3)
+    expect(Point3D.toString()).toEqual("Point3D")
+    expect(a).toEqual({ x:1, y:2, z:3 })
+    expect(a.x == 1 && a.y == 2 && a.z == 3).toEqual(true)
+    expect(a.toString()).toEqual("Point3D(1,2,3)")
+    expect(Point3D.is(a)).toEqual(true)
+    Point3D.prototype.scale = function(n){
+        return Point3D(this.x * n, this.y * n, this.z * n)
+    }
+    const b = a.scale(2)
+    expect(b.toString()).toEqual("Point3D(2,4,6)")
+    const c = Point3D.from({y: 2, x: 1, z: 3})
+    expect(c).toEqual({ x: 1, y: 2, z: 3 })
+    expect(c.toString()).toEqual("Point3D(1,2,3)")
+    expect(() => {
+        Point3D(1,2)
+    }).toThrow(TypeError);
+})
 
-const Option = sum("Option", {
-    Some: ["x"],
-    None: []
-});
-
-let temp = Option.Some(10);
-console.log(temp.toString());
-console.log(Option.is(temp));
-console.log(Option.is({}));
-console.log(Option.Some.is(temp));
-console.log(Option.None.is(Option.None));
-
-Option.prototype.map = function(f) {
-    return this.cata({
-        Some: ({ x }) => Option.Some(f(x)),
-        None: () => this
+test('styp.sum', () => {
+    const Option = sum("Option", {
+        Some: ["x"],
+        None: []
     });
-}
-
-console.log("here!");
-console.log(temp.map(x => x*2).toString());
-console.log(Option.None.cata({
-    None: () => Option.Some(0)
-}).toString())
+    let temp = Option.Some(10);
+    expect(temp.toString()).toEqual("Option.Some(10)");
+    expect(Option.None.toString()).toEqual("Option.None");
+    expect(Option.is(temp)).toEqual(true);
+    expect(Option.is({})).toEqual(false);
+    expect(Option.Some.is(temp)).toEqual(true);
+    expect(Option.None.is(Option.None)).toEqual(true);
+    Option.prototype.map = function(f) {
+        return this.cata({
+            Some: ({ x }) => Option.Some(f(x)),
+            None: () => this
+        });
+    }
+    temp = temp.map(x => x*2);
+    expect(temp.toString()).toEqual("Option.Some(20)")
+    temp = Option.None.map(x => x*2);
+    expect(Option.None.is(temp)).toEqual(true)
+})
